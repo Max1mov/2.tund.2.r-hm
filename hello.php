b19dc6932295cee8381b4bc461aa6f0a6922fdd2
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2015-09-11 11:37:04 +0300

    second

--- hello.php
+++ hello.php
@@ -6,3 +6,20 @@
 	echo $first_name." ".$last_name;
 	
 ?>
+
+<br>
+<?php
+
+	$age = 5;
+	// kui on väiksem kui 18, siis alaealine
+	// kui on suurem siis täisealine
+	if($age <19) { 
+	//tõene
+	echo "alaealine";
+	
+	} else {
+		//väär
+	echo "alaealine";
+	}
+
+?>
